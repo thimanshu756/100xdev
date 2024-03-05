8ffd6796e2ec53f23a6437babb9fa8f7a999312c
thimanshu756 <himanshu1662.be22@gmail.com>
2024-03-05 23:38:58 +0530

    appbar component creates

---
+++ Week-11-Monorepo/apps/client/src/components/Appbar.tsx
@@ -0,0 +1,98 @@
+import {Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import { isUserLoading } from "store";
+import {useSetRecoilState, useRecoilValue} from "recoil";
+import { userState } from "store";
+import { userEmailState } from "store"
+import { useRouter } from 'next/navigation'
+
+export function Appbar({}) {
+    const userLoading = useRecoilValue(isUserLoading);
+    const userEmail = useRecoilValue(userEmailState);
+    const setUser = useSetRecoilState(userState);
+    const router = useRouter()
+
+    if (userLoading) {
+        return <></>
+    }
+
+    if (userEmail) {
+        return <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: 4,
+            zIndex: 1
+        }}>
+            <div style={{marginLeft: 10, cursor: "pointer"}} onClick={() => {
+                // navigate("/")
+            }}>
+                <Typography variant={"h6"}>Coursera</Typography>
+            </div>
+    
+            <div style={{display: "flex"}}>
+                <div style={{marginRight: 10, display: "flex"}}>
+                <div style={{marginRight: 10}}>
+                        <Button
+                            onClick={() => {
+                                router.push("/addcourse")
+                            }}
+                        >Add course</Button>
+                    </div>
+
+                    <div style={{marginRight: 10}}>
+                        <Button
+                            onClick={() => {
+                                router.push("/courses")
+                            }}
+                        >Courses</Button>
+                    </div>
+
+                    <Button
+                        variant={"contained"}
+                        onClick={() => {
+                            localStorage.setItem("token", null);
+                            setUser({
+                                isLoading: false,
+                                userEmail: null
+                            })
+                        }}
+                    >Logout</Button>
+                </div>
+            </div>
+        </div>
+    } else {
+        return <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: 4,
+            zIndex: 1
+        }}>
+            <div style={{marginLeft: 10, cursor: "pointer"}} onClick={() => {
+                // navigate("/")
+            }}>
+                <Typography variant={"h6"}>Coursera</Typography>
+            </div>
+    
+            <div style={{display: "flex"}}>
+                <div style={{marginRight: 10}}>
+                    <Button
+                        variant={"contained"}
+                        onClick={() => {
+                            router.push("/signup")
+                        }}
+                    >Signup</Button>
+                </div>
+                <div>
+                    <Button
+                        variant={"contained"}
+                        onClick={() => {
+                            // navigate("/signin")
+                        }}
+                    >Signin</Button>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Appbar;
